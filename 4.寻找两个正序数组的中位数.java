38f8719edcd361b70fd69f34fd930018fc8b5413
iponkan <iponkan@gmail.com>
2020-09-21 14:21:44 +0800

    java 分支

--- 4.寻找两个正序数组的中位数.java
+++ 4.寻找两个正序数组的中位数.java
@@ -1,5 +1,5 @@
 /*
- * @lc app=leetcode.cn id=4 lang=javascript
+ * @lc app=leetcode.cn id=4 lang=java
  *
  * [4] 寻找两个正序数组的中位数
  *
@@ -14,51 +14,36 @@
  * Testcase Example:  '[1,3]\n[2]'
  *
  * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
- *
+ * 
  * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
- *
+ * 
  * 你可以假设 nums1 和 nums2 不会同时为空。
- *
- *
- *
+ * 
+ * 
+ * 
  * 示例 1:
- *
+ * 
  * nums1 = [1, 3]
  * nums2 = [2]
- *
+ * 
  * 则中位数是 2.0
- *
- *
+ * 
+ * 
  * 示例 2:
- *
+ * 
  * nums1 = [1, 2]
  * nums2 = [3, 4]
- *
+ * 
  * 则中位数是 (2 + 3)/2 = 2.5
- *
- *
+ * 
+ * 
  */
 
 // @lc code=start
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
- */
-var findMedianSortedArrays = function (nums1, nums2) {
-  if (nums1.length > nums2.length) {
-    return findMedianSortedArrays(nums2, nums1);
-  }
-  const len = nums1.length + nums2.length;
-  let cut1 = 0;
-  let cut2 = 0;
-  //范围控制
-  const cutL = 0;
-  const cutR = nums1.length;
-
-  while (cut1 <= nums1.length) {
-    cut1 = (cutR - cutL) / 2 + cutL; //取到中间的数
-    cut2 = len / 2 - cut1;
-  }
-};
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        System.out.println("aaaa");
+        return 0;
+    }
+}
 // @lc code=end
